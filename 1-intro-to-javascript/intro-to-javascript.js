4a35b856cbddeeb6d5faaad56a3230ee223ff2b0
Olena Hryk <olena.hryk@gmail.com>
2023-06-16 01:46:09 +0300

    Added design & script for module 1

---
+++ 1-intro-to-javascript/intro-to-javascript.js
@@ -0,0 +1,129 @@
+// Task 1  --> script in HTML
+
+// Task 2
+document
+    .querySelector("#intro-to-javascript_task2")
+    .addEventListener("click", () => {
+        console.log("Hryk");
+    });
+
+
+// Task 3
+document
+    .querySelector("#intro-to-javascript_task3")
+    .addEventListener("click", () => {
+        let firstValue = "Hello";
+        let secondValue = 404;
+        outputVariables(firstValue, secondValue);
+        firstValue = secondValue;
+        outputVariables(firstValue, secondValue);
+
+        function outputVariables(valueFirst, valueSecond) {
+            setTimeout(function(){alert(`First value is: ${valueFirst} \nSecond value is: ${valueSecond}`)},100);
+        }
+    });
+
+
+// Task 4
+document
+    .querySelector("#intro-to-javascript_task4")
+    .addEventListener("click", () => {
+        const dataTypes = {
+            String: "SoftServe",
+            Number: 2023,
+            Boolean: true,
+            Undefined: undefined,
+            Null: null,
+        };
+        // console.log(typeof dataTypes.Undefined === 'undefined');
+        console.log(dataTypes);
+    });
+
+
+// Task 5
+document
+    .querySelector("#intro-to-javascript_task5")
+    .addEventListener("click", () => {
+        legalAge();
+
+        function legalAge() {
+            let isAdult = confirm("Are you of legal age?");
+            console.log(`${isAdult}: ${isAdult ? `The user is of legal age` : `The user is underage`}`);
+        }
+    });
+
+
+// Task 6
+document
+    .querySelector("#intro-to-javascript_task6")
+    .addEventListener("click", () => {
+        const NAME = "Olenka";
+        const SURNAME = "Hryk";
+        const STUDY_GROUP = "PI-2014";
+        const BIRTHDAY = 1997;
+        const IS_MARRIED = true;
+        let numberOfChildren = null;
+        let address;
+
+        const personalData = [
+            NAME,
+            SURNAME,
+            STUDY_GROUP,
+            BIRTHDAY,
+            IS_MARRIED,
+            numberOfChildren,
+            address,
+        ];
+
+        typeOfVariables(personalData);
+        console.warn(sortByType(personalData));
+
+        function typeOfVariables(arr) {
+            arr.forEach((element) => {
+                console.warn(`Type of ${element} is ${typeof element}`);
+            });
+        }
+
+        function sortByType(arr) {
+            const typeWeight = {
+                number: 1,
+                boolean: 2,
+                string: 3,
+                undefined: 4,
+                null: 5,
+            };
+            return arr.sort((a, b) => typeWeight[typeof a] - typeWeight[typeof b]);
+        }
+    });
+
+
+// Task 7
+document
+    .querySelector("#intro-to-javascript_task7")
+    .addEventListener("click", () => {
+        let login = prompt("Enter your login:", "User");
+        let email = prompt("Enter your email:", "usermail@gmail.com");
+        let password = prompt("Enter your password:", "qwerty");
+
+        alert(`Dear ${login}, your email is ${email}, your password is ${password}`);
+    });
+
+
+// Task 8
+document
+    .querySelector("#intro-to-javascript_task8")
+    .addEventListener("click", () => {
+        let secH = 3600;
+        let secD = 24 * secH;
+        let secM = 30 * secD;
+        let isWantToCalculate = confirm(`The number of seconds in an hour is ${secH}, in a day - ${secD}, in a month - ${secM}.\nDo you want to calculate your own data?`);
+        isWantToCalculate ? calculateSecInData(secH, secD, secM) : null;
+
+        function calculateSecInData(secH, secD, secM) {
+            let hour = prompt("Enter number of hours:", "3");
+            let day = prompt("Enter number of days:", "2");
+            let month = prompt("Enter number of month:", "4");
+
+            alert(`The number of seconds in ${hour} hour is ${secH * hour}, in ${day} day - ${secD * day}, in ${month} month - ${secM * month}.`);
+        }
+    });
