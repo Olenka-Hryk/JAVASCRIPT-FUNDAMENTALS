9f4476c37d21048306bdf9522dfd88e674344dc9
Olena Hryk <olena.hryk@gmail.com>
2023-06-16 21:49:46 +0300

    Added task conditions to module 2

---
+++ 2-operators-arrays/operators-arrays.js
@@ -0,0 +1,34 @@
+// Task 1
+function executeModule2Task1() {
+  alert("The script will be here soooooon");
+}
+
+// Task 2
+function executeModule2Task2() {
+  alert("The script will be here soooooon");
+}
+
+// Task 3
+function executeModule2Task3() {
+  alert("The script will be here soooooon");
+}
+
+// Task 4
+function executeModule2Task4() {
+  alert("The script will be here soooooon");
+}
+
+// Task 5
+function executeModule2Task5() {
+  alert("The script will be here soooooon");
+}
+
+// Task 6
+function executeModule2Task6() {
+  alert("The script will be here soooooon");
+}
+
+// Task 7
+function executeModule2Task7() {
+  alert("The script will be here soooooon");
+}
