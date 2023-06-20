78bdb8a17f23693bc76aab2e5512b22ed9b2c156
Olena Hryk <olena.hryk@gmail.com>
2023-06-20 21:13:40 +0300

    Added task conditions to Module 3

---
+++ 3-loops-functions/loops-functions.js
@@ -0,0 +1,39 @@
+// Task 1
+function executeModule3Task1() {
+  alert("The script will be here soooooon");
+}
+
+// Task 2
+function executeModule3Task2() {
+  alert("The script will be here soooooon");
+}
+
+// Task 3
+function executeModule3Task3() {
+  alert("The script will be here soooooon");
+}
+
+// Task 4
+function executeModule3Task4() {
+  alert("The script will be here soooooon");
+}
+
+// Task 5
+function executeModule3Task5() {
+  alert("The script will be here soooooon");
+}
+
+// Task 6
+function executeModule3Task6() {
+  alert("The script will be here soooooon");
+}
+
+// Task 7
+function executeModule3Task7() {
+  alert("The script will be here soooooon");
+}
+
+// Task 8
+function executeModule3Task8() {
+  alert("The script will be here soooooon");
+}
