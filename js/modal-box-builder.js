ea881c4c324cfcbf748af07d4348d14d779be920
Olena Hryk <olena.hryk@gmail.com>
2023-06-30 01:15:49 +0300

    [ Code refactoring ]

--- js/modal-box-builder.js
+++ js/modal-box-builder.js
@@ -1,105 +1,89 @@
 function showFormElements(arrayOfElements, block) {
     arrayOfElements.forEach((elem) => {
         if (elem.element === "label") {
-            var label = document.createElement("Label");
-            label.setAttribute("for", elem.for);
-            label.classList = "main-text form__label " + elem.classList;
-            label.innerHTML = elem.text;
-            block.appendChild(label);
-
-        }
-
-        if (elem.element === "input") {
-            let input = document.createElement("INPUT");
-            input.id = elem.id;
-            input.setAttribute("placeholder", elem.placeholder);
-            input.classList = "accent-text form__input " + elem.classList;
-            input.type = elem.type;
-            block.appendChild(input);
-        }
-
-        if (elem.element === "p") {
-            let p = document.createElement("p");
-            p.id = elem.id;
-            p.classList = "accent-text form__result form__result--total " + elem.classList;
-            p.innerHTML = elem.text;
-            block.appendChild(p);
-        }
-
-        if (elem.element === "p" && elem.classList === "form__result--task") {
-            let p = document.createElement("p");
-            p.id = elem.id;
-            p.classList = "accent-text form__result " + elem.classList;
-            p.innerHTML = elem.text;
-            block.appendChild(p);
-        }
-
-        if (elem.element === "button") {
-            let button = document.createElement("button");
-            button.id = elem.id;
-            button.type = "button";
-            button.classList = "button " + elem.classList;
-            button.innerHTML = elem.text;
-            button.addEventListener('click', elem.onclick);
-            block.appendChild(button);
-        }
-
-        if (elem.element === "select") {
-            let select = document.createElement("select");
-            select.id = elem.id;
-            select.classList = "form__select " + elem.classList;
-            block.appendChild(select);
-        }
-
-        if (elem.element === "option") {
-            let option = document.createElement("option");
-            option.id = elem.id;
-            option.classList = " " + elem.classList;
-            option.value = elem.value;
-            option.innerHTML = elem.text;
-            block.appendChild(option);
+            addLabel(elem, block);
+        } else if (elem.element === "input") {
+            addInput(elem, block);
+        } else if (elem.element === "p") {
+            addP(elem, block);
+        } else if (elem.element === "button") {
+            addButton(elem, block);
+        } else if (elem.element === "select") {
+            addSelect(elem, block);
+        } else if (elem.element === "option") {
+            addOption(elem, block);
         }
     });
 }
 
+function addLabel(elem, container) {
+    const label = document.createElement("label");
+    label.setAttribute("for", elem.for);
+    label.classList.add("main-text", "form__label");
+    if (elem.classList) label.classList.add(elem.classList);
+    label.innerHTML = elem.text;
+    container.appendChild(label);
+}
 
+function addInput(elem, container) {
+    let input = document.createElement("input");
+    input.id = elem.id;
+    input.setAttribute("placeholder", elem.placeholder);
+    input.classList.add("accent-text", "form__input");
+    if (elem.classList) input.classList.add(elem.classList);
+    input.type = elem.type;
+    container.appendChild(input);
+}
 
-// const listDomTaskElement = [{
-//     element: "label",
-//     for: "income-desired",
-//     classList: "",
-//     text: "Desired income:"
-// },
-// {
-//     element: "input",
-//     id: "income-desired",
-//     placeholder: "2000$",
-//     classList: "",
-//     type: "number"
-// },
-// {
-//     element: "p",
-//     id: "result-num-line-code",
-//     classList: "",
-//     text: "Result: "
-// },
-// {
-//     element: "button",
-//     id: "execute-task-income",
-//     classList: "button--form-ex",
-//     text: "Execute",
-//     onclick: () => { fffffffffffffffff() }
-// }
-// {
-//     element: "select",
-//     id: "choose-op-module3-task6",
-//     classList: "",
-//   },
-//   {
-//     element: "option",
-//     id: "op-subtraction-module3-task6",
-//     classList: "",
-//     value: "subtraction",
-//     text: "subtraction",
-//   },
-// ]
+function addP(elem, container) {
+    if (elem.classList.includes("form__result--task")) {
+        let p = document.createElement("p");
+        p.id = elem.id;
+        p.classList.add("accent-text", "form__result");
+        if (elem.classList) p.classList.add(elem.classList);
+        p.innerHTML = elem.text;
+        container.appendChild(p);
+    } else if (elem.classList.includes("form__result--task-part")) {
+        let p = document.createElement("p");
+        p.id = elem.id;
+        p.classList.add("accent-text", "form__result");
+        if (elem.classList) p.classList.add(elem.classList);
+        p.innerHTML = elem.text;
+        container.appendChild(p);
+    } else {
+        let p = document.createElement("p");
+        p.id = elem.id;
+        p.classList.add("accent-text", "form__result", "form__result--total");
+        if (elem.classList) p.classList.add(elem.classList);
+        p.innerHTML = elem.text;
+        container.appendChild(p);
+    }
+}
+
+function addButton(elem, container) {
+    let button = document.createElement("button");
+    button.id = elem.id;
+    button.type = "button";
+    button.classList.add("button");
+    if (elem.classList) button.classList.add(elem.classList);
+    button.innerHTML = elem.text;
+    button.addEventListener("click", elem.onclick);
+    container.appendChild(button);
+}
+
+function addSelect(elem, container) {
+    let select = document.createElement("select");
+    select.id = elem.id;
+    select.classList.add("form__select");
+    if (elem.classList) select.classList.add(elem.classList);
+    container.appendChild(select);
+}
+
+function addOption(elem, container) {
+    let option = document.createElement("option");
+    option.id = elem.id;
+    if (elem.classList) option.classList.add(elem.classList);
+    option.value = elem.value;
+    option.innerHTML = elem.text;
+    container.appendChild(option);
+}
